142878baa70faa676bf8de23ff3ac3588b867345
Brianna <bs3864@nyu.edu>
2019-04-07 07:37:31 -0700

    Adding Menu Code

---
+++ CodelabMidterm/Assets/Scripts/GameManger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+	title,
+	about,
+	pause,
+	playing
+}
+
+public class GameManger : MonoBehaviour
+{
+	private GameState currentState;
+	private GameObject pause;
+	private GameObject[] titleScreenObj;
+	private GameObject[] aboutScreenObj;
+
+	public static GameManger instance; 
+	// Use this for initialization
+	void Start () {
+		
+		//Singleton Make sure there's only one GM 
+		if (instance  == null)
+		{
+			
+		}
+		else
+		{
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void checkGameState()
+	{
+		switch (currentState)
+		{
+			case GameState.title:
+				break;
+			case GameState.about:
+				break;
+			case GameState.playing:
+				break;
+			case  GameState.pause:
+				break;
+		}
+	}
+}
